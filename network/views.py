1390e456a3f99303847d808e41d385c929ae3c68
Vika-207 <111182393+Vika-207@users.noreply.github.com>
2022-12-08 14:16:07 +0200

    Create views.py

---
+++ network/views.py
@@ -0,0 +1,64 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .models import User
+
+
+def index(request):
+    return render(request, "network/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["Byrka"]
+        password = request.POST["lera_1308"]
+        user = authenticate(request, username=Byrka, password=lera_1308)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "network/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "network/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["Byrka"]
+        email = request.POST["byrka.viktorijia@gmail.com"]
+
+        # Ensure password matches confirmation
+        password = request.POST["lera_1308"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "network/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(Byrka, byrka.viktorijia@gmail.com, lera_1308)
+            user.save()
+        except IntegrityError:
+            return render(request, "network/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/register.html")
+
